9aee9e86f93dcfe7358eef3dd14fc3ce0a171005
Kayla <6lackmacaroon@gmail.com>
2019-04-11 16:44:56 -0600

    BANANA

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,20 +1,21 @@
 
 class TabLink {
-  constructor(link) {
+  constructor(banana) {
     // Assign this.element to the passed in DOM element
-  this.link = link;
-    
+    this.banana = banana;
+
     // Get the custom data attribute on the Link
-  this.data = this.link.dataset.tab;
-    
+    this.data = this.banana.dataset.tab;
+    //console.log(this.link.dataset.tab);
+
     // Using the custom data attribute get the associated Item element
-  this.item = document.querySelector(`.tabs-items[data-tab="${this.data}"]`);
-    
+    this.item = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+
     // Using the Item element, create a new instance of the TabItem class
-  this.tabItem = new TabItem(this.item);
-    
+    this.tabItem = new TabItem(this.item);
+
     // Add a click event listener on this instance, calling the select method on click
-  this.tabItem.addEventListener('click', this.select.bind(this));
+    this.banana.addEventListener('click', this.select.bind(this));
 
   };
 
@@ -24,33 +25,37 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    Array.from(links).forEach(function(items){
-      items.classList.remove('tabs-link-selected');
+    Array.from(links).forEach(function (banana) {
+      banana.classList.remove('tabs-link-selected');
     });
     // Add a class named "tabs-link-selected" to this link
-    this.item.classList.add('tabs-link-selected');
-    
+    this.banana.classList.add('tabs-link-selected');
+
     // Call the select method on the item associated with this link
-    
+    this.tabItem.select();
   }
 }
 
 class TabItem {
-  constructor(item) {
+  constructor(banana) {
     // Assign this.element to the passed in element
-  this.item = item;
+    this.banana = banana;
+    console.log(this.banana);
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-  const items = document.querySelectorAll('');
+    const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    
+    Array.from(items).forEach(function (banana) {
+      banana.classList.remove('tabs-item-selected');
+    });
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.banana.classList.add('tabs-item-selected');
   }
 }
+
 
 /* START HERE: 
 - Select all classes named ".tabs-link" and assign that value to the links variable
@@ -59,6 +64,6 @@ class TabItem {
 */
 
 const links = document.querySelectorAll('.tabs-link');
-links.forEach(function(link){
-  new TabLink(link);
+links.forEach(function (banana) {
+  new TabLink(banana);
 });
